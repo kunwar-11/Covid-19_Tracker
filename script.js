95e6896c6e514adf0285b9f509075fdf04b73ee9
kunwar-11 <kunwarrishabh70@gmail.com>
2020-12-10 20:56:44 +0530

    added functionality

--- script.js
+++ script.js
@@ -186,10 +186,10 @@ function getTable(data) {
     }
 }
 function indianSys(x) {
-    var lastThree = x.substring(x.length-3);
+    var lastThreeNo = x.substring(x.length-3);
     var otherNumbers = x.substring(0,x.length-3);
     if(otherNumbers != '')
-        lastThree = ',' + lastThree;
-    var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+        lastThreeNo = ',' + lastThreeNo;
+    var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThreeNo;
     return res;
 }
